8c0953468f9f72519a5a078beae6c6caf7107d21
Taiwo Obadare <taiwo.obadare@gmail.com>
2021-12-07 13:47:58 +0000

    procfile

--- blog_site/urls.py
+++ blog_site/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('website.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
