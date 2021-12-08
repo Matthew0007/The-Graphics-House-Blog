9b8407a8d210cc627c24b6d788430cfb5736958c
Taiwo Obadare <taiwo.obadare@gmail.com>
2021-12-08 22:44:36 +0000

    sqlite

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -99,12 +99,8 @@ WSGI_APPLICATION = 'blog_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'convert_db',
-        'USER' : 'postgres',
-        'PASSWORD' : '1255',
-        'HOST' : 'localhost',
-        'PORT' : '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
